a0b5083b1e16591c75f92655fa7d1354d1c0e1e2
bridger <bridgerjones15@gmail.com>
2021-12-01 16:55:20 -0700

    stuff

--- mobility/servos/servos.py
+++ mobility/servos/servos.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
+
 # GPIO INITIALIZATION
 FRONT_SERVO_PIN = 4
 SERVO_UP_DOWN_PIN = 13
@@ -10,6 +11,7 @@ SERVO_LEFT_RIGHT_PIN = 14
 #Set the GPIO port to BCM encoding mode
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 GPIO.setup(FRONT_SERVO_PIN,GPIO.OUT,initial=GPIO.HIGH)
 
 GPIO.setup(SERVO_UP_DOWN_PIN,GPIO.OUT,initial=GPIO.HIGH)
@@ -34,4 +36,6 @@ def servo_pulse(servo_pin, angle):
   GPIO.output(servo_pin, GPIO.LOW)
   time.sleep(20 - pulse_width / 1000)
 
-servo_pulse(SERVO_LEFT_RIGHT_PIN, 90)
+
+while True:
+    servo_pulse(SERVO_LEFT_RIGHT_PIN, 90)
